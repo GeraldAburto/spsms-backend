7b5108b81b71550bd277f781bd8c566f550e3ad6
GeraldAburto <aburtogerald@gmail.com>
2024-08-20 22:54:59 -0600

    :sparkles: Add register user use case

---
+++ src/v1/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
+
+import UserAlreadyExistsException from '@/core/users/exceptions/user-already-exists.exception';
+import RegisterUserUseCase from '@/core/users/use-cases/register-user.use-case';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller({
+  path: 'users',
+  version: '1',
+})
+export class UsersController {
+  constructor(private readonly registerUserUseCase: RegisterUserUseCase) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    try {
+      await this.registerUserUseCase.execute({
+        email: createUserDto.email,
+        firstName: createUserDto.firstName,
+        lastName: createUserDto.lastName,
+        password: createUserDto.password,
+      });
+    } catch (error) {
+      if (error instanceof UserAlreadyExistsException) {
+        throw new BadRequestException(error.message);
+      }
+
+      throw new InternalServerErrorException();
+    }
+  }
+}
