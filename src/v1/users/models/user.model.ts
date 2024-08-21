468be2c4d1697fd82d045cfb0a362b2a231e72e4
GeraldAburto <aburtogerald@gmail.com>
2024-08-21 13:35:44 -0600

    :sparkles: Integrate Sequelize

---
+++ src/v1/users/models/user.model.ts
@@ -0,0 +1,51 @@
+import {
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+type UserModelCreationAttributes = Pick<
+  UserModel,
+  'email' | 'firstName' | 'lastName' | 'passwordHash'
+>;
+
+@Table({
+  tableName: 'users',
+  underscored: true,
+  timestamps: false,
+})
+export class UserModel extends Model<UserModel, UserModelCreationAttributes> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  firstName: string;
+
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  lastName: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  passwordHash: string;
+}
---
+++ src/v1/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import User from '@/core/users/user.entity';
+import { IUserRepository } from '@/core/users/user.repository';
+import { UserModel } from './models/user.model';
+
+@Injectable()
+export class UsersService implements IUserRepository {
+  constructor(
+    @InjectModel(UserModel) private readonly userModel: typeof UserModel,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.userModel.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) return null;
+
+    return new User(
+      user.email,
+      user.firstName,
+      user.id,
+      user.lastName,
+      user.passwordHash,
+    );
+  }
+
+  async save(user: User): Promise<void> {
+    await this.userModel.create({
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      passwordHash: user.password,
+    });
+  }
+}
