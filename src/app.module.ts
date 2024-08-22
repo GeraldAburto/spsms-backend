07a0e7ba40dc3b81477a9c8c73087c3a490c8ef5
GeraldAburto <aburtogerald@gmail.com>
2024-08-22 10:39:08 -0600

    :wrench: Validate env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 import sequelizeConfig from '@/config/sequelize.config';
+import { validate } from '@/env.validation';
 import { UserModel } from '@/v1/users/models/user.model';
 import { UsersModule } from '@/v1/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validate,
+    }),
     SequelizeModule.forRoot({ ...sequelizeConfig(), models: [UserModel] }),
     UsersModule,
   ],
