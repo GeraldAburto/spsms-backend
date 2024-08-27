949e7eb76850c0f1175ba1e01dfc922f9ba0ff1b
GeraldAburto <aburtogerald@gmail.com>
2024-08-27 11:21:36 -0600

    :memo: Setting up OpenApi documentation using Swagger

--- src/v1/users/dto/create-user.dto.ts
+++ src/v1/users/dto/create-user.dto.ts
@@ -1,15 +1,40 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    example: 'Jane',
+    minLength: 1,
+    maxLength: 50,
+    required: true,
+  })
   @IsNotEmpty()
+  @MaxLength(50)
   firstName: string;
 
+  @ApiProperty({
+    type: String,
+    example: 'Doe',
+    minLength: 1,
+    maxLength: 50,
+    required: true,
+  })
   @IsNotEmpty()
+  @MaxLength(50)
   lastName: string;
 
+  @ApiProperty({ type: String, example: 'janedoe@mail.com', required: true })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    type: String,
+    example: '$Abc1234#',
+    minLength: 8,
+    required: true,
+  })
   @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
