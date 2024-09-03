c057d847d1bf592c4dcfbf5b122335a23c0a8d45
GeraldAburto <aburtogerald@gmail.com>
2024-09-03 13:37:06 -0600

    :memo: Change Swagger docs URL

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.listen(3000);
 }
